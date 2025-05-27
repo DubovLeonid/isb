c6b2d64f954d358d47cedf5a7c764b6b016d0733
DubovLeonid <163322693+DubovLeonid@users.noreply.github.com>
2025-05-28 00:57:36 +0400

    Update Source.cpp

--- lab2/Source.cpp
+++ lab2/Source.cpp
@@ -3,10 +3,19 @@
 #include <fstream>
 
 int main() {
+/**
+     * @details
+     * 1. Использует генератор случайных чисел (`std::random_device`)
+     * 2. Применяет вихрь Мерсенна (`std::mt19937`) для генерации
+     * 3. Сохраняет результат в файл
+     *
+     * @return 0 при успешном выполнении
+*/
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> dis(0, 1);
     const int N = 128;
+
     std::ofstream out("cpp_random_bits.txt");
     for (int i = 0; i < N; ++i) {
         out << dis(gen);
